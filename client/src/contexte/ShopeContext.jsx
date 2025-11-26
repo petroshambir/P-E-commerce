0ff8addeefcb76dce63ee94616cb4aac1e09f9ce
petros <petroshambirr@gmail.com>
2025-11-26 19:52:07 +0300

    url

--- client/src/contexte/ShopeContext.jsx
+++ client/src/contexte/ShopeContext.jsx
@@ -11,8 +11,8 @@ const ShopContextProvider = (props)=>{
 
     const currency = '$';
     const delivery_fee =10;
-     const backendUrl =" https://p-e-commerce.onrender.com";
-    //  const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
+    //  const backendUrl =" https://p-e-commerce.onrender.com";
+     const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
     const[search,setSearch]=useState('');
     const[showSearch,setShowSearch]=useState(false);
     const [cartItem,setCartItem]= useState({});
